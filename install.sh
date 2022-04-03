344cbc0f61361ec2d35b4266fcf60ee1f48b8370
小尨 <43496459+Jason6111@users.noreply.github.com>
2022-04-03 19:31:50 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -114,26 +114,11 @@ start_menu
 
 #安装x-ui
 x-ui() {
-echo "=============================================================="
-echo "选择版本"
-echo "1.原版"
-echo "2.魔改版"
-echo "0.退出"
-echo "=============================================================="
-	read -r -p "请选择:" installx-ui
-	if [[ "${installx-ui}" == "1" ]]; then
-		bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh) 
-	else
-	  [[ "${installx-ui}" == "2" ]]; 
-		bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
-                rm -f /usr/local/x-ui/bin/xray-linux-amd64
-                wget --no-check-certificate -O /usr/local/x-ui/bin/xray-linux-amd64 "https://cdn.jsdelivr.net/gh/Jason6111/ExpressSetup@main/xray-linux-amd64"
-                sudo chmod 755 /usr/local/x-ui/bin/xray-linux-amd64
-                systemctl restart x-ui
-	else	
-	  [[ "${installx-ui}" == "3" ]]; 
-	  start_menu
-	fi
+bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
+rm -f /usr/local/x-ui/bin/xray-linux-amd64
+wget --no-check-certificate -O /usr/local/x-ui/bin/xray-linux-amd64 "https://cdn.jsdelivr.net/gh/Jason6111/ExpressSetup@main/xray-linux-amd64"
+sudo chmod 755 /usr/local/x-ui/bin/xray-linux-amd64
+systemctl restart x-ui
 start_menu
 }
 
