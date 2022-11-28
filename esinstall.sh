3b4af53c7420ae264141541a7a1567bf83750ba2
小尨 <43496459+Jason6111@users.noreply.github.com>
2022-11-28 12:14:16 +0800

    Update esinstall.sh

--- esinstall.sh
+++ esinstall.sh
@@ -189,7 +189,7 @@ wget -N https://raw.githubusercontent.com/Jason6111/ExpressSetup/main/hysteria/h
 }
 
 BT() {
-wget -O install.sh http://www.aapanel.com/script/install-ubuntu_6.0_en.sh && sudo bash install.sh aapanel || wget -O install.sh http://www.aapanel.com/script/install-ubuntu_6.0_en.sh && bash install.sh aapanel || yum install -y wget && wget -O install.sh http://www.aapanel.com/script/install_6.0_en.sh && bash install.sh aapanel
+wget -O install.sh http://www.aapanel.com/script/install-ubuntu_6.0_en.sh && sudo bash install.sh aapanel 
 }
 
 zhuanfa() {
