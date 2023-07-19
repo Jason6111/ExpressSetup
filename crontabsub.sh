c64879db3202ec33b6a2cf076cf367776a5d7d6a
小尨 <43496459+Jason6111@users.noreply.github.com>
2023-07-19 13:33:33 +0800

    Update crontabsub.sh

--- crontabsub.sh
+++ crontabsub.sh
@@ -7,7 +7,9 @@ domain2_content=$(curl -s https://www.example2.com | base64 -d)
 domain3_content=$(curl -s https://www.example3.com | base64 -d)
 
 # 合并内容为一个字符串
-combined_content="$domain1_content$domain2_content$domain3_content"
+combined_content="$domain1_content
+$domain2_content
+$domain3_content"
 
 # 将合并后的内容进行64base编码
 encoded_content=$(echo -n "$combined_content" | base64)
