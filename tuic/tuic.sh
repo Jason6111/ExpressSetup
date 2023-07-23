eae3c8feda80124d70e74e7057afe2fae4573c04
小尨 <43496459+Jason6111@users.noreply.github.com>
2023-07-23 18:50:48 +0800

    Update tuic.sh

--- tuic/tuic.sh
+++ tuic/tuic.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-tuV="22.11.23 V 1.0"
+tuV="23.7.23 V 2.0"
 remoteV=`wget -qO- https://raw.githubusercontent.com/Jason6111/ExpressSetup/main/tuic/tuic.sh | sed  -n 2p | cut -d '"' -f 2`
 chmod +x /root/tuic.sh
 ln -sf /root/tuic.sh /usr/bin/tu
@@ -29,7 +29,7 @@ elif cat /proc/version | grep -q -E -i "ubuntu"; then
 release="Ubuntu"
 elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
 release="Centos"
-else 
+else
 red "不支持你当前系统，请选择使用Ubuntu,Debian,Centos系统。" && exit
 fi
 vsid=`grep -i version_id /etc/os-release | cut -d \" -f2 | cut -d . -f1`
@@ -58,14 +58,14 @@ vi=`systemd-detect-virt`
 start(){
 if [[ $vi = openvz ]]; then
 TUN=$(cat /dev/net/tun 2>&1)
-if [[ ! $TUN =~ 'in bad state' ]] && [[ ! $TUN =~ '处于错误状态' ]] && [[ ! $TUN =~ 'Die Dateizugriffsnummer ist in schlechter Verfassung' ]]; then 
+if [[ ! $TUN =~ 'in bad state' ]] && [[ ! $TUN =~ '处于错误状态' ]] && [[ ! $TUN =~ 'Die Dateizugriffsnummer ist in schlechter Verfassung' ]]; then
 red "检测到未开启TUN，现尝试添加TUN支持" && sleep 2
 cd /dev
 mkdir net
 mknod net/tun c 10 200
 chmod 0666 net/tun
 TUN=$(cat /dev/net/tun 2>&1)
-if [[ ! $TUN =~ 'in bad state' ]] && [[ ! $TUN =~ '处于错误状态' ]] && [[ ! $TUN =~ 'Die Dateizugriffsnummer ist in schlechter Verfassung' ]]; then 
+if [[ ! $TUN =~ 'in bad state' ]] && [[ ! $TUN =~ '处于错误状态' ]] && [[ ! $TUN =~ 'Die Dateizugriffsnummer ist in schlechter Verfassung' ]]; then
 green "添加TUN支持失败，建议与VPS厂商沟通或后台设置开启" && exit
 else
 green "恭喜，添加TUN支持成功，现添加TUN守护功能" && sleep 4
@@ -110,37 +110,6 @@ systemctl disable apache2 >/dev/null 2>&1
 fi
 }
 
-wgcfgo(){
-wgcfv6=$(curl -s6m5 https://www.cloudflare.com/cdn-cgi/trace -k | grep warp | cut -d= -f2)
-wgcfv4=$(curl -s4m5 https://www.cloudflare.com/cdn-cgi/trace -k | grep warp | cut -d= -f2)
-if [[ ! $wgcfv4 =~ on|plus && ! $wgcfv6 =~ on|plus ]]; then
-sureipadress
-else
-systemctl stop wg-quick@wgcf >/dev/null 2>&1
-kill -15 $(pgrep warp-go) >/dev/null 2>&1 && sleep 2
-sureipadress
-systemctl start wg-quick@wgcf >/dev/null 2>&1
-systemctl restart warp-go >/dev/null 2>&1
-systemctl enable warp-go >/dev/null 2>&1
-systemctl start warp-go >/dev/null 2>&1
-fi
-}
-
-insupdate(){
-if [[ $release = Centos ]]; then
-if [[ ${vsid} =~ 8 ]]; then
-cd /etc/yum.repos.d/ && mkdir backup && mv *repo backup/ 
-curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-8.repo
-sed -i -e "s|mirrors.cloud.aliyuncs.com|mirrors.aliyun.com|g " /etc/yum.repos.d/CentOS-*
-sed -i -e "s|releasever|releasever-stream|g" /etc/yum.repos.d/CentOS-*
-yum clean all && yum makecache
-fi
-yum install epel-release -y
-else
-apt update
-fi
-}
-
 instucore(){
 lastvsion=`curl -s https://data.jsdelivr.com/v1/package/gh/EAimTY/tuic | sed -n 30p | tr -d ',"'`
 wget https://github.com/EAimTY/tuic/releases/download/${lastvsion}/${lastvsion}-${bit}-unknown-linux-musl -O /usr/local/bin/tuic
@@ -150,157 +119,6 @@ blue "成功安装tuic内核版本：$(/usr/local/bin/tuic -v)\n"
 else
 red "安装tuic内核失败" && rm -rf tuic.sh && exit
 fi
-}
-
-inscertificate(){
-green "tuic协议证书申请方式选择如下:"
-readp "1. acme一键申请证书脚本（支持常规80端口模式与dns api模式），已用此脚本申请的证书则自动识别（回车默认）\n2. 自定义证书路径（非/root/ca路径）\n请选择：" certificate
-if [ -z "${certificate}" ] || [ $certificate == "1" ]; then
-if [[ -f /root/ca/cert.crt && -f /root/ca/private.key ]] && [[ -s /root/ca/cert.crt && -s /root/ca/private.key ]] && [[ -f /root/ca/ca.log ]]; then
-blue "经检测，之前已使用此acme脚本申请过证书"
-readp "1. 直接使用原来的证书（回车默认）\n2. 删除原来的证书，重新申请证书\n请选择：" certacme
-if [ -z "${certacme}" ] || [ $certacme == "1" ]; then
-ym=$(cat /root/ca/ca.log)
-blue "检测到的域名：$ym ，已直接引用\n"
-elif [ $certacme == "2" ]; then
-curl https://get.acme.sh | sh
-bash /root/.acme.sh/acme.sh --uninstall
-rm -rf /root/ca
-rm -rf ~/.acme.sh acme.sh
-sed -i '/--cron/d' /etc/crontab
-[[ -z $(/root/.acme.sh/acme.sh -v 2>/dev/null) ]] && green "acme.sh卸载完毕" || red "acme.sh卸载失败"
-sleep 2
-wget -N https://raw.githubusercontent.com/Jason6111/ExpressSetup/main/acme.sh && bash acme.sh
-ym=$(cat /root/ca/ca.log)
-if [[ ! -f /root/ca/cert.crt && ! -f /root/ca/private.key ]] && [[ ! -s /root/ca/cert.crt && ! -s /root/ca/private.key ]]; then
-red "证书申请失败，脚本退出" && exit
-fi
-fi
-else
-wget -N https://raw.githubusercontent.com/Jason6111/ExpressSetup/main/acme.sh && bash acme.sh
-ym=$(cat /root/ca/ca.log)
-if [[ ! -f /root/ca/cert.crt && ! -f /root/ca/private.key ]] && [[ ! -s /root/ca/cert.crt && ! -s /root/ca/private.key ]]; then
-red "证书申请失败，脚本退出" && exit
-fi
-fi
-certificatec='/root/ca/cert.crt'
-certificatep='/root/ca/private.key'
-elif [ $certificate == "2" ]; then
-readp "请输入已放置好的公钥文件crt的路径（/a/b/……/cert.crt）：" cerroad
-blue "公钥文件crt的路径：$cerroad "
-readp "请输入已放置好的密钥文件key的路径（/a/b/……/private.key）：" keyroad
-blue "密钥文件key的路径：$keyroad "
-certificatec=$cerroad
-certificatep=$keyroad
-readp "请输入已解析好的域名:" ym
-blue "已解析好的域名：$ym "
-else 
-red "输入错误，请重新选择" && inscertificate
-fi
-}
-
-insport(){
-readp "\n设置tuic端口[1-65535]（回车跳过为2000-65535之间的随机端口）：" port
-if [[ -z $port ]]; then
-port=$(shuf -i 2000-65535 -n 1)
-until [[ -z $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$port") ]]
-do
-[[ -n $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$port") ]] && yellow "\n端口被占用，请重新输入端口" && readp "自定义tuic端口:" port
-done
-else
-until [[ -z $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$port") ]]
-do
-[[ -n $(ss -tunlp | grep -w udp | awk '{print $5}' | sed 's/.*://g' | grep -w "$port") ]] && yellow "\n端口被占用，请重新输入端口" && readp "自定义tuic端口:" port
-done
-fi
-blue "已确认端口：$port\n"
-}
-
-inspswd(){
-readp "设置tuic令牌码Token，必须为6位字符以上（回车跳过为随机6位字符）：" pswd
-if [[ -z ${pswd} ]]; then
-pswd=`date +%s%N |md5sum | cut -c 1-6`
-else
-if [[ 6 -ge ${#pswd} ]]; then
-until [[ 6 -le ${#pswd} ]]
-do
-[[ 6 -ge ${#pswd} ]] && yellow "\n用户名必须为6位字符以上！请重新输入" && readp "\n设置tuic令牌码Token：" pswd
-done
-fi
-fi
-blue "已确认令牌码Token：${pswd}\n"
-}
-
-insconfig(){
-green "设置tuic的配置文件、服务进程……\n"
-sureipadress(){
-ip=$(curl -s4m6 ip.sb -k) || ip=$(curl -s6m6 ip.sb -k)
-}
-wgcfgo
-mkdir /etc/tuic >/dev/null 2>&1
-cat <<EOF > /etc/tuic/tuic.json
-{
-    "port": $port,
-    "token": ["$pswd"],
-    "certificate": "$certificatec",
-    "private_key": "$certificatep",
-    "ip": "::",
-    "congestion_controller": "bbr",
-    "alpn": ["h3"]
-}
-EOF
-mkdir /root/tuic >/dev/null 2>&1
-cat <<EOF > /root/tuic/v2rayn.json
-{
-    "relay": {
-        "server": "$ym",
-        "port": $port,
-        "token": "$pswd",
-        "ip": "$ip",
-        "congestion_controller": "bbr",
-        "udp_relay_mode": "quic",
-        "alpn": ["h3"],
-        "disable_sni": false,
-        "reduce_rtt": false,
-        "max_udp_relay_packet_size": 1500
-    },
-    "local": {
-        "port": 6080,
-        "ip": "127.0.0.1"
-    },
-    "log_level": "off"
-}
-EOF
-
-cat <<EOF > /root/tuic/tuic.txt
-Sagernet 与 小火箭 配置说明（以下6项必填）：
-{
-服务器地址：$ym
-服务器端口：$port
-令牌码token：$pswd
-应用层协议ALPN：h3
-UDP转发模式：开启
-congestion controller模式：bbr
-}
-EOF
-
-cat << EOF >/etc/systemd/system/tuic.service
-[Unit]
-Description=TUIC
-Documentation=https://github.com/Jason6111/ExpressSetup/tree/main/tuic
-After=network.target
-[Service]
-User=root
-ExecStart=/usr/local/bin/tuic -c /etc/tuic/tuic.json
-Restart=on-failure
-RestartSec=10
-LimitNOFILE=infinity
-[Install]
-WantedBy=multi-user.target
-EOF
-systemctl daemon-reload
-systemctl enable tuic
-systemctl start tuic
 }
 
 stclre(){
@@ -341,15 +159,15 @@ elif [ $choose == "4" ];then
 inscertificate
 oldcer=`cat /etc/tuic/tuic.json 2>/dev/null | sed -n 4p | awk '{print $2}' | tr -d ',"'`
 oldkey=`cat /etc/tuic/tuic.json 2>/dev/null | sed -n 5p | awk '{print $2}' | tr -d ',"'`
-sed -i "s#$oldcer#${certificatec}#g" /etc/tuic/tuic.json
-sed -i "s#$oldkey#${certificatep}#g" /etc/tuic/tuic.json
-oldym=`cat /root/tuic/v2rayn.json 2>/dev/null | sed -n 3p | awk '{print $2}' | tr -d ',"'`
-sed -i "s/$oldym/${ym}/g" /root/tuic/v2rayn.json
-sed -i "3s/$oldym/${ym}/g" /root/tuic/tuic.txt
+sed -i "s#$oldcer#${cert_path}#g" /etc/tuic/tuic.json
+sed -i "s#$oldkey#${key_path}#g" /etc/tuic/tuic.json
+olddomain=`cat /root/tuic/v2rayn.json 2>/dev/null | sed -n 3p | awk '{print $2}' | tr -d ',"'`
+sed -i "s/$olddomain/${domain}/g" /root/tuic/v2rayn.json
+sed -i "3s/$olddomain/${domain}/g" /root/tuic/tuic.txt
 susstuic
 elif [ $choose == "5" ];then
 tu
-else 
+else
 red "请重新选择" && changeserv
 fi
 }
@@ -366,7 +184,7 @@ changeuuid(){
     sed -i "s/$olduuid/$uuid/g" /root/tuic/v2rayn.json
     sed -i "s/$olduuid/$uuid/g" /root/tuic/tuic.txt
     sed -i "s/$olduuid/$uuid/g" /root/tuic/clash-meta.yaml
-    
+
     systemctl stop tuic && systemctl start tuic
 }
 
@@ -417,7 +235,7 @@ wget -N --no-check-certificate https://gitlab.com/rwkgyg/cfwarp/raw/main/CFwarp.
 }
 
 tuicstatus(){
-wgcfv6=$(curl -s6m5 https://www.cloudflare.com/cdn-cgi/trace -k | grep warp | cut -d= -f2) 
+wgcfv6=$(curl -s6m5 https://www.cloudflare.com/cdn-cgi/trace -k | grep warp | cut -d= -f2)
 wgcfv4=$(curl -s4m5 https://www.cloudflare.com/cdn-cgi/trace -k | grep warp | cut -d= -f2)
 [[ ! $wgcfv4 =~ on|plus && ! $wgcfv6 =~ on|plus ]] && wgcf=$(green "未启用") || wgcf=$(green "启用中")
 if [[ -n $(systemctl status tuic 2>/dev/null | grep -w active) && -f '/etc/tuic/tuic.json' ]]; then
@@ -434,7 +252,7 @@ if [[ -z $(systemctl status tuic 2>/dev/null | grep -w active) && ! -f '/etc/tui
 red "未正常安装tuic" && exit
 fi
 wget -N https://raw.githubusercontent.com/Jason6111/ExpressSetup/main/tuic/tuic.sh
-chmod +x /root/tuic.sh 
+chmod +x /root/tuic.sh
 ln -sf /root/tuic.sh /usr/bin/tu
 green "tuic安装脚本升级成功" && tu
 }
@@ -455,15 +273,6 @@ systemctl disable tuic >/dev/null 2>&1
 rm -f /etc/systemd/system/tuic.service
 rm -rf /usr/local/bin/tuic /etc/tuic /root/tuic /root/tuic.sh /usr/bin/tu
 green "tuic卸载完成！"
-}
-
-susstuic(){
-systemctl restart tuic
-if [[ -n $(systemctl status tuic 2>/dev/null | grep -w active) && -f '/etc/tuic/tuic.json' ]]; then
-green "tuic服务启动成功" && tuicshare
-else
-red "tuic服务启动失败，请运行systemctl status tuic查看服务状态并反馈，脚本退出" && exit
-fi
 }
 
 tuicshare(){
@@ -499,8 +308,8 @@ instuic(){
         red "Tuic 内核安装失败！"
         exit 1
     fi
-    
-    green "Tuic 协议证书申请方式如下："
+
+  green "Tuic 协议证书申请方式如下："
     echo ""
     echo -e " ${GREEN}1.${PLAIN} 脚本自动申请 ${YELLOW}（默认）${PLAIN}"
     echo -e " ${GREEN}2.${PLAIN} 自定义证书路径"
@@ -520,60 +329,35 @@ instuic(){
             domain=$(cat /root/ca/ca.log)
             green "检测到原有域名：$domain 的证书，正在应用"
         else
-            WARPv4Status=$(curl -s4m8 https://www.cloudflare.com/cdn-cgi/trace -k | grep warp | cut -d= -f2)
-            WARPv6Status=$(curl -s6m8 https://www.cloudflare.com/cdn-cgi/trace -k | grep warp | cut -d= -f2)
-            if [[ $WARPv4Status =~ on|plus ]] || [[ $WARPv6Status =~ on|plus ]]; then
-                wg-quick down wgcf >/dev/null 2>&1
-                systemctl stop warp-go >/dev/null 2>&1
-                ip=$(curl -s4m8 ip.p3terx.com -k | sed -n 1p) || ip=$(curl -s6m8 ip.p3terx.com -k | sed -n 1p)
-                wg-quick up wgcf >/dev/null 2>&1
-                systemctl start warp-go >/dev/null 2>&1
-            else
-                ip=$(curl -s4m8 ip.p3terx.com -k | sed -n 1p) || ip=$(curl -s6m8 ip.p3terx.com -k | sed -n 1p)
-            fi
-            
-            read -p "请输入需要申请证书的域名：" domain
-            [[ -z $domain ]] && red "未输入域名，无法执行操作！" && exit 1
-            green "已输入的域名：$domain" && sleep 1
-            domainIP=$(curl -sm8 ipget.net/?ip="${domain}")
-            if [[ $domainIP == $ip ]]; then
-                ${PACKAGE_INSTALL[int]} curl wget sudo socat openssl
-                if [[ $SYSTEM == "CentOS" ]]; then
-                    ${PACKAGE_INSTALL[int]} cronie
-                    systemctl start crond
-                    systemctl enable crond
-                else
-                    ${PACKAGE_INSTALL[int]} cron
-                    systemctl start cron
-                    systemctl enable cron
-                fi
-                curl https://get.acme.sh | sh -s email=$(date +%s%N | md5sum | cut -c 1-16)@gmail.com
-                source ~/.bashrc
-                bash ~/.acme.sh/acme.sh --upgrade --auto-upgrade
-                bash ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
-                if [[ -n $(echo $ip | grep ":") ]]; then
-                    bash ~/.acme.sh/acme.sh --issue -d ${domain} --standalone -k ec-256 --listen-v6 --insecure
-                else
-                    bash ~/.acme.sh/acme.sh --issue -d ${domain} --standalone -k ec-256 --insecure
-                fi
-                bash ~/.acme.sh/acme.sh --install-cert -d ${domain} --key-file /root/ca/private.key --fullchain-file /root/ca/cert.crt --ecc
-                if [[ -f /root/ca/cert.crt && -f /root/ca/private.key ]] && [[ -s /root/ca/cert.crt && -s /root/ca/private.key ]]; then
-                    echo $domain > /root/ca/ca.log
-                    sed -i '/--cron/d' /etc/crontab >/dev/null 2>&1
-                    echo "0 0 * * * root bash /root/.acme.sh/acme.sh --cron -f >/dev/null 2>&1" >> /etc/crontab
-                    green "证书申请成功! 脚本申请到的证书 (cert.crt) 和私钥 (private.key) 文件已保存到 /root 文件夹下"
-                    yellow "证书crt文件路径如下: /root/ca/cert.crt"
-                    yellow "私钥key文件路径如下: /root/ca/private.key"
-                fi
-            else
-                red "当前域名解析的IP与当前VPS使用的真实IP不匹配"
-                green "建议如下："
-                yellow "1. 请确保CloudFlare小云朵为关闭状态(仅限DNS), 其他域名解析或CDN网站设置同理"
-                yellow "2. 请检查DNS解析设置的IP是否为VPS的真实IP"
-                yellow "3. 脚本可能跟不上时代, 建议截图发布到GitHub Issues、GitLab Issues、论坛或TG群询问"
-            fi
-        fi
-    fi
+            if [[ -f /root/ca/cert.crt && -f /root/ca/private.key ]] && [[ -s /root/ca/cert.crt && -s /root/ca/private.key ]] && [[ -f /root/ca/ca.log ]]; then
+blue "经检测，之前已使用此acme脚本申请过证书"
+readp "1. 直接使用原来的证书（回车默认）\n2. 删除原来的证书，重新申请证书\n请选择：" certacme
+if [ -z "${certacme}" ] || [ $certacme == "1" ];
+then domain=$(cat /root/ca/ca.log)
+blue "检测到的域名：$domain ，已直接引用\n"
+elif [ $certacme == "2" ]; then
+curl https://get.acme.sh | sh
+bash /root/.acme.sh/acme.sh --uninstall
+rm -rf /root/ca
+rm -rf ~/.acme.sh acme.sh
+sed -i '/--cron/d' /etc/crontab
+[[ -z $(/root/.acme.sh/acme.sh -v 2>/dev/null) ]] && green "acme.sh卸载完毕" || red "acme.sh卸载失败"
+sleep 2
+wget -N https://raw.githubusercontent.com/Jason6111/ExpressSetup/main/acme.sh && bash acme.sh
+domain=$(cat /root/ca/ca.log)
+if [[ ! -f /root/ca/cert.crt && ! -f /root/ca/private.key ]] && [[ ! -s /root/yca/cert.crt && ! -s /root/ca/private.key ]]; then
+red "证书申请失败，脚本退出" && exit
+fi
+fi
+else
+wget -N https://raw.githubusercontent.com/Jason6111/ExpressSetup/main/acme.sh && bash acme.sh
+domain=$(cat /root/ca/ca.log)
+if [[ ! -f /root/ca/cert.crt && ! -f /root/ca/private.key ]] && [[ ! -s /root/ca/cert.crt && ! -s /root/ca/private.key ]]; then
+red "证书申请失败，脚本退出" && exit
+fi
+fi
+fi
+fi
 
     read -p "设置 tuic 端口[1-65535]（回车则随机分配端口）：" port
     [[ -z $port ]] && port=$(shuf -i 2000-65535 -n 1)
@@ -672,7 +456,7 @@ proxy-groups:
     type: select
     proxies:
       - Misaka-tuic
-      
+
 rules:
   - GEOIP,CN,DIRECT
   - MATCH,Proxy
@@ -708,16 +492,17 @@ EOF
     yellow "Clash Meta 客户端配置文件已保存到 /root/tuic/clash-meta.yaml"
     yellow "Tuic 节点配置明文如下，并保存到 /root/tuic/tuic.txt"
     cat /root/tuic/tuic.txt
+    sleep 2
 }
 start_menu(){
 tuicstatus
 clear
 red "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-green "  1. 安装tuic（必选）" 
+green "  1. 安装tuic（必选）"
 green "  2. 卸载tuic"
 white "----------------------------------------------------------------------------------"
-green "  3. 变更配置（端口、令牌码Token、证书）" 
-green "  4. 关闭、开启、重启tuic"   
+green "  3. 变更配置（端口、令牌码Token、证书）"
+green "  4. 关闭、开启、重启tuic"
 green "  5. 更新tuic安装脚本"
 green "  6. 更新tuic内核版本"
 white "----------------------------------------------------------------------------------"
@@ -746,17 +531,17 @@ white "操作系统：  $(blue "$op")" && white "内核版本：  $(blue "$versi
 white "$status"
 echo
 readp "请输入数字:" Input
-case "$Input" in     
+case "$Input" in
  1 ) instuic;;
  2 ) unins;;
  3 ) changeserv;;
  4 ) stclre;;
- 5 ) uptuicj;; 
+ 5 ) uptuicj;;
  6 ) uptuic;;
  7 ) tuicshare;;
  8 ) acme;;
  9 ) cfwarp;;
- * ) exit 
+ * ) exit
 esac
 }
 if [ $# == 0 ]; then
